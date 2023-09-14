46949d47ffd1ae103bb6527ce91903df6bda5e08
Jayjay422 <segunadediran422@gmail.com>
2023-09-14 15:02:28 -0700

    lol

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 /**
  * _isupper - check if c is upper
  *
- * c: input  alphabet
+ * @c: input for alphabet
  *
- * Return: 1 if its uppercase, 0 if not uppercase
+ * Return: 1 if its uppercase, 0 if not
 */
 
 int _isupper(int c)
