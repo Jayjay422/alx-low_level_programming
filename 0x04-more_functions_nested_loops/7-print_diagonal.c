618c23ef4b799aa909ad5d1154873cf415a676cc
Jayjay422 <segunadediran422@gmail.com>
2023-09-14 15:49:13 -0700

    lol

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,23 +9,18 @@
 
 void print_diagonal(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	int postn, space;
 
-    int spaces = 0;
-    int i;
-
-    while (n > 0)
-    {
-        for (i = 0; i < spaces; i++)
-            _putchar(' ');
-        _putchar('\\');
-        _putchar('\n');
-        spaces++;
-        n--;
-    }
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; ++postn)
+		{
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '\' char*/
+			_putchar('\n');
+		}
+	}
 }
-
