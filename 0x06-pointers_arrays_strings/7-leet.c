36ab8072de45058a6df49c245c7ef1d8e948eb80
Jayjay422 <segunadediran422@gmail.com>
2023-09-20 08:41:15 -0700

    lol

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,31 @@
+#include "main.h"
+
 /**
- * leet - a function that encodes a string into 1337
- *
- * @s: string input
- *
- * Return: @s
-*/
-
-char *leet(char *s)
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
+ */
+char *cap_string(char *s)
 {
-	int i, c = 0;
-	int sl[] = {97, 101, 111, 116, 108};
-	int ul[] = {65, 69, 79, 84, 76};
-	int n[] = {52, 51, 48, 55, 49};
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	/*iterate values in array s*/
-	while (s[c] != '\0')
+	while (*(s + i))
 	{
-		/**
-		 * loop through array value five times
-		 * to check if the value is equal to
-		 * any small letter in array sl or
-		 * upper letter in array ul and if it
-		 * is replace it with the value in array n
-		*/
-		for (i = 0; i < 5; i++)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (s[c] == sl[i] || s[c] == ul[i])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				s[c] = n[i];
-				break;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
-		c++;
+		i++;
 	}
 	return (s);
 }
